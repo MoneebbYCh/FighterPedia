e2c30d41461133adc57e3beb0feed8fff1c9aeab
Moneebb Yasin Chaudhary <moneebb@gmail.com>
2024-07-15 22:10:57 +0500

    Added flags and completed Dretails page

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect,useState } from 'react';
+import React, { useRef, useEffect,useState } from 'react';
 import { View, Text, Image, StyleSheet, Animated, Button, TouchableOpacity, FlatList, ScrollView, ImageBackground} from 'react-native';
 import LogoImage from '../FighterPedia/FPstuff/file.png';
 
@@ -94,6 +94,66 @@ function FighterCollection (){
   )
 }
 
+const Drawer = ({ title, children }) => {
+  const [expanded, setExpanded] = useState(false);
+  const animation = useRef(new Animated.Value(0)).current;
+
+  const toggleDrawer = () => {
+    setExpanded(!expanded);
+    Animated.timing(animation, {
+      toValue: expanded ? 0 : 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const heightInterpolation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 220], // Adjust to desired height
+  });
+
+  return (
+    <View style={styles.drawerContainer}>
+      <TouchableOpacity onPress={toggleDrawer} style={styles.drawerButton}>
+        <Text style={styles.drawerButtonText}>{title}</Text>
+      </TouchableOpacity>
+      <Animated.View style={[styles.drawerContent, { height: heightInterpolation }]}>
+        {children}
+      </Animated.View>
+    </View>
+  );
+};
+
+const Drawer2 = ({ title, children }) => {
+  const [expanded, setExpanded] = useState(false);
+  const animation = useRef(new Animated.Value(0)).current;
+
+  const toggleDrawer = () => {
+    setExpanded(!expanded);
+    Animated.timing(animation, {
+      toValue: expanded ? 0 : 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const heightInterpolation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 280], // Adjust to desired height
+  });
+
+  return (
+    <View style={styles.drawerContainer}>
+      <TouchableOpacity onPress={toggleDrawer} style={styles.drawerButton}>
+        <Text style={styles.drawerButtonText}>{title}</Text>
+      </TouchableOpacity>
+      <Animated.View style={[styles.drawerContent, { height: heightInterpolation }]}>
+        {children}
+      </Animated.View>
+    </View>
+  );
+};
+
 
 
 function PlaneDetailBF109() {
@@ -101,14 +161,96 @@ function PlaneDetailBF109() {
     <ImageBackground source={require('./FPstuff/Bf109Details.jpg')} style={styles.background}>
         <View style={styles.overlay}>
 
+
         <View style={styles.DetailImageContainer}>
         <Image source={require('./FPstuff/MesserschmittBf109/im6.png')} style={styles.DetailImage}/>
         </View>
 
         <View style={styles.HorizontalDetailContainer}>
         <Text style = {styles.DetailHeading}> Operators </Text>
-
+        <ScrollView horizontal = {true} contentContainerStyle={styles.scrollcontainer}>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/germany.png')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Hungary.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Bulgaria.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Croatia.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Finland.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Italy.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/ItalianSR.png')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Romania.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/SlovakR.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/SpanishState.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Switzerland.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/yugoslavia.jpg')} style={styles.image} />
+                </View>
+        </ScrollView>
         </View>
+
+        <Drawer2 title="General Information">
+          <Text style={styles.drawerText}>Role: 		Fighter{"\n"}
+Manufacturer:	Bayerische Flugzeugwerke and Messerschmitt{"\n"}
+Designed by: 	Willy Messerschmitt{"\n"}
+First flight: 		28 May 1935{"\n"}
+Introduced: 		1937{"\n"}
+Retired: 		1945, Luftwaffe and 1965, Spain{"\n"}
+Status: 		Retired{"\n"}
+Primary users:{"\t"}- Luftwaffe{"\n"}
+{"\t"}{"\t"}{"\t"}{"\t"}-Hungarian Air Force{"\n"}
+{"\t"}{"\t"}{"\t"}{"\t"}-Italian Social Republic Air Force{"\n"}
+{"\t"}{"\t"}{"\t"}{"\t"}-Romanian Air Force{"\n"}
+Number built:	more than 33,000{"\n"}
+Variants:{"\t"}- Avia S-99/S-199{"\n"}
+{"\t"}{"\t"}{"\t"}- Hispano Aviacion{"\n"} 
+{"\t"}{"\t"}{"\t"}- Ha 1112 </Text>
+        </Drawer2>
+        <Drawer2 title="General Characterisistics">
+          <Text style={styles.drawerText}>
+          * Crew: One{"\n"}
+    * Length: 8.95 m (29 ft 7 in){"\n"}
+    * Wingspan: 9.925 m (32 ft 6 in){"\n"}
+    * Height: 2.60 m (8 ft 2 in){"\n"}
+    * Wing area: 16.05 m² (173.3 ft²){"\n"}
+    * Empty weight: 2,247 kg (5,893 lb){"\n"}
+    * Loaded weight: 3148 kg (6,940 lb){"\n"}
+    * Max takeoff weight: 3,400 kg (7,495 lb){"\n"}
+    * Powerplant: 1× Daimler-Benz DB 605A-1 liquid-cooled inverted V12, 1,475 PS (1,455 hp, 1,085 kW){"\n"}
+          </Text>
+        </Drawer2>
+        <Drawer2 title="Performance">
+          <Text style={styles.drawerText}>
+          * Maximum speed: 640 km/h (398 mph) at 6,300m (20,669ft){"\n"}
+    * Cruise speed: 590 km/h (365 mph) at 6,000 m (19,680 ft){"\n"}
+    * Range: 850 km (528 mi) with droptank 1,000 km (620 mi){"\n"}
+    * Service ceiling: 12,000 m (39,370 ft){"\n"}
+    * Rate of climb: 17.0 m/s (3,345 ft/min){"\n"}
+    * Wing loading: 199.8 kg/m² (40.9 lb/ft²){"\n"}
+    * Power/mass: 330 W/kg (0.21 hp/lb){"\n"}
+          </Text>
+        </Drawer2>
+
+        
 
       </View>
     </ImageBackground>
@@ -125,10 +267,62 @@ function PlaneDetailBF110 (){
 
         <View style={styles.HorizontalDetailContainer}>
         <Text style = {styles.DetailHeading}> Operators </Text>
+        <ScrollView horizontal = {true} contentContainerStyle={styles.scrollcontainer}>
                 <View style={styles.circle}>
                 <Image source={require('./FPstuff/germany.png')} style={styles.image} />
                 </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Hungary.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Italy.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Romania.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/SovietUnion.jpg')} style={styles.image} />
+                </View>
+                <View style={styles.circle}>
+                <Image source={require('./FPstuff/Croatia.jpg')} style={styles.image} />
+                </View>
+        </ScrollView>
         </View>
+
+        <Drawer title="General Information">
+          <Text style={styles.drawerText}>Role:			Heavy fighter/Fighter-bomber/Night fighter{"\n"}
+                                          Manufacturer: 	Bayerische Flugzeugwerke and Messerschmitt{"\n"}
+                                            Designed by: 	Willy Messerschmitt{"\n"}
+                                            First flight: 		12 May 1936{"\n"}
+                                            Introduced: 		1937{"\n"}
+                                            Retired: 		1945 (Luftwaffe){"\n"}
+                                            Primary users:{"\t"}- Luftwaffe{"\n"}
+                                            {"\t"}{"\t"}{"\t"}{"\t"}- Hungarian Air Force{"\n"}
+                                            {"\t"}{"\t"}{"\t"}{"\t"}- Regia Aeronautica{"\n"}
+                                            {"\t"}{"\t"}{"\t"}{"\t"}- Romanian Air Force{"\n"}
+                                            Number built 	6,170 </Text>
+        </Drawer>
+        <Drawer title="General Characterisistics">
+          <Text style={styles.drawerText}>
+          * Crew: 2 (3 for night fighter variants){"\n"}
+    * Length: 12.3 m (40 ft 6 in){"\n"}
+    * Wingspan: 16.3 m (53 ft 4 in){"\n"}
+    * Height: 3.3 m (10 ft 9 in){"\n"}
+    * Wing area: 38.8 m² (414 ft²){"\n"}
+    * Empty weight: 4,500 kg (9,900 lb){"\n"}
+    * Loaded weight: 6,700 kg (14,800 lb){"\n"}
+    * Powerplant: 2× Daimler-Benz DB 601B-1 liquid-cooled inverted V-12, 809 kW (1,100 hp) each
+          </Text>
+        </Drawer>
+        <Drawer title="Performance">
+          <Text style={styles.drawerText}>
+          * Maximum speed: 560 km/h (348 mph){"\n"}
+    * Range: 2,410 km combat, 2,800 km ferry (1,500 mi / 1,750 mi){"\n"}
+    * Service ceiling: 10,500 m (35,000 ft){"\n"}
+    * Wing loading: 173 kg/m² (35.7 lb/ft²){"\n"}
+    * Power/mass: 0.3644 kW/kg (0.155 hp/lb){"\n"}
+          </Text>
+        </Drawer>
 
       </View>
     </ImageBackground>
@@ -558,12 +752,36 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   circle: {
-    width: 50, // Adjust size of the circle
-    height: 50, // Adjust size of the circle
-    borderRadius: 25, // Half of the width/height for a perfect circle
-    backgroundColor: 'lightgray', // Optional background color for the circle
-    justifyContent: 'center', // Center vertically
-    alignItems: 'center', // Center horizontally
+    width: 70, 
+    height: 70, 
+    borderRadius: 20, 
+    backgroundColor: 'lightgray', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    marginRight : 20,
+  },
+  drawerContainer: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'dark-grey',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+
+  },
+  drawerButton: {
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    padding: 15,
+  },
+  drawerButtonText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  drawerContent: {
+    overflow: 'hidden',
+  },
+  drawerText: {
+    padding: 15,
+    color: 'white',
   },
 });
 
